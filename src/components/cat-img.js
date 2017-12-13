c0850c96e53f1fe4ed4eef67d4fd31afcd4bc785
Julien Lengrand-Lambert <Julien.Lengrand-Lambert@ing.nl>
2017-12-13 13:45:28 +0100

    Step 1 : Simple Custom Element

---
+++ src/components/cat-img.js
@@ -0,0 +1,19 @@
+class CatImgElement extends HTMLElement {
+
+    constructor(){
+        super()
+    }    
+
+    connectedCallback(){
+        this.innerHTML = `<h1>Hello Meetup !</h1>`;
+        console.log("I'm here!");
+    }
+
+    disconnectedCallback(){
+        console.log("I'm gone!");
+    }
+}
+
+window.customElements.define(
+                'cat-img', 
+                    CatImgElement);
